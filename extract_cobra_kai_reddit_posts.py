e0ddcb26f7aec2edefa742e15f31dfb844b24143
PurpleFlower436 <bhargvih12@gmail.com>
2026-04-15 23:51:35 -0500

    Added more Cobra Kai season 6 finale reddit posts to the main dataset and added bucket filtering logic by year in the append_cobra_kai_season_6_finale_data_to_csv function

--- extract_cobra_kai_reddit_posts.py
+++ extract_cobra_kai_reddit_posts.py
@@ -1,7 +1,9 @@
 import csv
-
-
-
+import requests
+import time
+from datetime import datetime
+import random
+headers = {"User-Agent": "cobra-kai-sentiment-analysis/1.0"}
 
 def create_headers_for_csv():
     cobra_kai_s6_finale_data_headers = [["post_id", "title", "body", "created_utc", "score", 
@@ -17,11 +19,35 @@ def create_headers_for_csv():
 
 
 
-def extract_data_from_cobra_kai_finale_posts():
-    with open('cobra_kai_finale_post_links.txt', 'r') as file:
+def append_cobra_kai_season_6_finale_data_to_csv():
+    with open('cobra_kai_s6_finale_post_links.txt', 'r') as file:
         cobra_kai_reddit_post_links = file.read().splitlines()
     
+    for link in cobra_kai_reddit_post_links:
+        url = link + ".json"
+
+        
+        
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        post = data[0]["data"]["children"][0]["data"]
+        utc_timestamp = post["created_utc"]
+        dt = datetime.utcfromtimestamp(utc_timestamp)
+        csv_writer = csv.writer(file)
+        if dt.year == 2025:
+            csv_writer.writerows(post["id"], post["title"], post["selftext"], post["created_utc"], post["ups"],
+            post["num_comments"], post["url"], "immediate")
+        else:
+            csv_writer.writerows(post["id"], post["title"], post["selftext"], post["created_utc"], post["ups"],
+            post["num_comments"], post["url"], "long_term")
+            
+
+
+        time.sleep(random.uniform(12, 18))
+
+    
     
 
 
-create_headers_for_csv()
+append_cobra_kai_season_6_finale_data_to_csv()
